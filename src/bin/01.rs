71c67517cf09aa5ac726b8d8ab71b8b539d14df8
Mario Ravalli <mario@raval.li>
2023-12-29 00:02:37 +0100

    Restore Test Value for CI

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -77,13 +77,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(54968));
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(142));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
-        assert_eq!(result, Some(54094));
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(281));
     }
 }
