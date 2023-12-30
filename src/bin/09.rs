96e8484808ee6e03adb48f40216a02107f7054ba
Mario Ravalli <mario@raval.li>
2023-12-30 14:46:23 +0100

    Day 9 - part 2

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -6,16 +6,25 @@ pub fn part_one(input: &str) -> Option<i32> {
             .split_whitespace()
             .map(|x| x.parse().unwrap())
             .collect();
-        acc += next_value(seq.clone()) + seq.last().unwrap();
+        acc += next_value(seq.clone(), true) + seq.last().unwrap();
         acc
     }))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i32> {
+    Some(input.lines().fold(0, |mut acc, line| {
+        let seq: Vec<i32> = line
+            .split_whitespace()
+            .map(|x| x.parse().unwrap())
+            .collect();
+        let pre = next_value(seq.clone(), false);
+        acc += seq.first().unwrap() - pre;
+
+        acc
+    }))
 }
 
-fn next_value(seq: Vec<i32>) -> i32 {
+fn next_value(seq: Vec<i32>, spin: bool) -> i32 {
     let mut _seq: Vec<i32> = Vec::new();
     let mut i = 0;
     while i < seq.len() - 1 {
@@ -27,7 +36,11 @@ fn next_value(seq: Vec<i32>) -> i32 {
     if _seq.iter().min().unwrap() == _seq.iter().max().unwrap() && _seq.last().unwrap() == &0 {
         return 0;
     }
-    next_value(_seq.clone()) + *_seq.last().unwrap()
+    if spin {
+        return next_value(_seq.clone(), spin) + *_seq.last().unwrap();
+    } else {
+        return *_seq.first().unwrap() - crate::next_value(_seq.clone(), spin);
+    }
 }
 
 #[cfg(test)]
@@ -42,7 +55,7 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file("inputs", DAY));
+        assert_eq!(result, Some(2));
     }
 }
